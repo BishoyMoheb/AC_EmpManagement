563df3ca6d419684d15a45767abc5435dd514e1b
BishoyMoheb <75308234+BishoyMoheb@users.noreply.github.com>
2026-08-18 17:01:08 +0300

    Add files via upload

---
+++ wwwroot/js/JS_ConfirmDeletion.js
@@ -0,0 +1,12 @@
+﻿function ConfirmDelete(unique_UserID, IsClicked) {
+    var DelRequest = 'DeleteRequest_' + unique_UserID;
+    var DelConfirmation = 'DeleteConfirmation_' + unique_UserID;
+    if (IsClicked) {
+        $('#' + DelRequest).hide();
+        $('#' + DelConfirmation).show();
+    }
+    else {
+        $('#' + DelRequest).show();
+        $('#' + DelConfirmation).hide();
+    }
+}
